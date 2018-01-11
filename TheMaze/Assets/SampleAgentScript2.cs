8bd23abbd423fe63b2428d336e233efbe8e249f7
Nicolas <nicolasbillod@gmail.com>
2018-01-11 14:54:41 +0100

    Interaction avec les agents

---
+++ TheMaze/Assets/SampleAgentScript2.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SampleAgentScript2 : MonoBehaviour {
+
+
+	public Transform target;
+	NavMeshAgent agent;
+	bool end;
+
+	// Use this for initialization
+	void Start () {
+		agent = GetComponent<NavMeshAgent> ();
+		target = GameObject.Find ("FloorTest2").transform;
+		end = false;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (end == true) {
+			target = GameObject.Find ("FloorTest").transform;
+		}
+		agent.SetDestination (target.position);
+
+		if (!agent.pathPending){
+			if (agent.remainingDistance <= agent.stoppingDistance){
+				if (!agent.hasPath || agent.velocity.sqrMagnitude == 0f){
+					end = true;
+				}
+			}
+		}
+
+
+	}
+}
