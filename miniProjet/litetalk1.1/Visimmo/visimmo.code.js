1a3a0e2cdb42801fe5c7c0a80c63c7b83234bbe6
Tanguy SOTO <tanguy.soto@etu.upmc.fr>
2018-01-29 22:02:10 +0100

    Timer, enchainement room, win/lost, bar satisfaction, preferences des bot sur les rooms

--- miniProjet/litetalk1.1/Visimmo/visimmo.code.js
+++ miniProjet/litetalk1.1/Visimmo/visimmo.code.js
@@ -1,27 +1,26 @@
+// ========= TOPICS ==========
 var marcTopic = [
 	// CODE INFO
 	[["KEY", "_class"],							["VAL", "bot"], ["BOT","marcBot"]],
 	[["KEY", "_reference"],					["VAL", ["m","marc","husband","father"]]],
 	[["KEY", "_htmlprefix"],				["VAL", "marc"]],
-	[["KEY", "_read"],							["VAL", ["userTopic","aurelieTopic","jeremyTopic"]]],
-	[["KEY", "_write"],							["VAL", ["userTopic"]]],
-	[["KEY", "_exec"],							["VAL", ["userTopic"]]], // try
+	[["KEY", "_read"],							["VAL", ["houseTopic","userTopic","aurelieTopic","jeremyTopic"]]],
 	
 	// BASICS INFOS
 	[["KEY", "type"],								["VAL", ["human","adult"]]],
 	
 	[["KEY", "name"],								["VAL", "Marc"],   				
-																	["WHY","my parents gave it to me. Actually, I am very happy about it."]],
+																	["WHY","My parents gave it to me. Actually, I am very happy about it."]],
 																	
 	[["KEY", "age"],								["VAL", 35], ["TYPE","INT"],
-																	["ONASK", "I am thirty-five year old."], 
+																	["ONASK", "35 year old."], 
 																	["WHY","I was born thirty-five years ago. Are you dumb ?"]],
 																	
 	[["KEY", "gender"],							["VAL", "male"],
 																	["ONASK", function(s) { return ((s == "male") ? "I am proud to be a male!" : "Just a female.") }]],
 																	
-	[["KEY", ["job"]],			        ["VAL", "I am a banker."]],
-	[["KEY", ["home","location"]],	["VAL", "We live in Normal Heights, San Diego."]],
+	[["KEY", ["job"]],			        ["VAL", "Banker"], ["WHY","I work in a bank..."]],
+	[["KEY", ["home","location"]],	["VAL", "Normal Heights, San Diego."]],
 	
 	// RELATIONS
 	[["KEY", "relative"],						["VAL", ["wife","son"]],
@@ -29,21 +28,20 @@ var marcTopic = [
 																	 
 	[["KEY", "wife"],								["VAL", "aurelieTopic"],["CAT","REL"]],
 	[["KEY", "son"],								["VAL", "jeremyTopic"],["CAT","REL"]],
-	
-	// FEELINGS
-	[["KEY", "happiness"],		["VAL",  0.8], ["CAT","VAR"], ["TYPE","INT"]], // 7 standard feelings initiated
-	[["KEY", "confidence"],		["VAL", -0.8], ["CAT","VAR"], ["TYPE","INT"]],
-	[["KEY", "irritability"],	["VAL",  0.8], ["CAT","VAR"], ["TYPE","INT"]],
-	[["KEY", "satisfaction"],	["VAL", -0.8], ["CAT","VAR"], ["TYPE","INT"]],
-	[["KEY", "respect"],			["VAL", -0.8], ["CAT","VAR"], ["TYPE","INT"]],
-	[["KEY", "force"],				["VAL", 0], ["CAT","VAR"], ["TYPE","INT"]],
-	[["KEY", "excitement"],		["VAL", 0], ["CAT","VAR"], ["TYPE","INT"]],
+
+	// FUNC
+	[["KEY", "action"],				["VAL", ["sentiment","look"]],
+														["ONASK",BOT_printActionList]],
+
+	[["KEY", "sentiment"], 		["VAL", "sentimentMarc"], ["CAT", "ACT"],
+														["EFFECT","I will give you my sentiment about the house"]],
+
+	[["KEY", "look"], 				["VAL", "lookMarc"], ["CAT", "ACT"],
+														["EFFECT","I will look at the elements in the room you talked about."]],
 	
 	// PREFS
-	[["KEY", "preference"],		["VAL", []], ["CAT","VAR"], ["ONASK",BOT_printPreferenceList]],  
-	[["KEY", "distaste"],			["VAL", []],  ["CAT","VAR"],["ONASK",BOT_printDistasteList]], 
-	[["KEY", "suggestion"],		["VAL", []], ["CAT","VAR"], ["ONASK",BOT_printSuggestionList]], 
-	[["KEY", "intention"],		["VAL", []], ["CAT","VAR"], ["ONASK",BOT_printIntentionList]],  
+	[["KEY", "preference"],		["VAL", [["houseTopic","exterior"],["houseTopic","kitchen"],["houseTopic","first bedroom"]]], ["CAT","VAR"], ["ONASK",BOT_printPreferenceList]],  
+	[["KEY", "distaste"],			["VAL", [["houseTopic","livingroom"]]],  ["CAT","VAR"],["ONASK",BOT_printDistasteList]],
 ];
 
 var aurelieTopic = [
@@ -51,24 +49,22 @@ var aurelieTopic = [
 	[["KEY", "_class"],							["VAL", "bot"], ["BOT","aurelieBot"]],
 	[["KEY", "_reference"],					["VAL", ["a","aurelie","wife","mother"]]],
 	[["KEY", "_htmlprefix"],				["VAL", "aurelie"]], //prefix of HTML elements  
-	[["KEY", "_read"],							["VAL", ["userTopic","marcTopic","jeremyTopic"]]],
-	[["KEY", "_write"],							["VAL", ["userTopic"]]],
-	[["KEY", "_exec"],							["VAL", ["userTopic"]]], // try
+	[["KEY", "_read"],							["VAL", ["houseTopic","userTopic","marcTopic","jeremyTopic"]]],
 	
 	// BASICS INFOS
 	[["KEY", "type"],								["VAL", ["human","adult"]]],
 	
 	[["KEY", "name"],								["VAL", "Aurélie"],   				
-																	["WHY","my parents gave it to me. Actually, I am very happy about it."]],
+																	["WHY","My parents gave it to me. Actually, I am very happy about it."]],
 																	
 	[["KEY", "age"],								["VAL", 37], ["TYPE","INT"],
-																	["ONASK", "I am thirty-seven year old."], 
+																	["ONASK", "37 year old."], 
 																	["WHY","I was born thirty-seven years ago."]],
 																	
 	[["KEY", "gender"],							["VAL", "female"],
 																	["ONASK", function(s) { return ((s == "male") ? "I am proud to be a male!" : "Just a female.") }]],
 																	
-	[["KEY", ["job"]],			        ["VAL", "I am a barber."]],
+	[["KEY", ["job"]],			        ["VAL", "Barber."]],
 	[["KEY", ["home","location"]],	["VAL", "We live in Normal Heights, San Diego"]],
 	
 	// RELATIONS
@@ -77,21 +73,20 @@ var aurelieTopic = [
 																	 
 	[["KEY", "husband"],						["VAL", "marcTopic"],["CAT","REL"]],
 	[["KEY", "son"],								["VAL", "jeremyTopic"],["CAT","REL"]],
-	
-	// FEELINGS
-	[["KEY", "happiness"],		["VAL",  0.8], ["CAT","VAR"], ["TYPE","INT"]], // 7 standard feelings initiated
-	[["KEY", "confidence"],		["VAL", -0.8], ["CAT","VAR"], ["TYPE","INT"]],
-	[["KEY", "irritability"],	["VAL",  0.8], ["CAT","VAR"], ["TYPE","INT"]],
-	[["KEY", "satisfaction"],	["VAL", -0.8], ["CAT","VAR"], ["TYPE","INT"]],
-	[["KEY", "respect"],			["VAL", -0.8], ["CAT","VAR"], ["TYPE","INT"]],
-	[["KEY", "force"],				["VAL", 0], ["CAT","VAR"], ["TYPE","INT"]],
-	[["KEY", "excitement"],		["VAL", 0], ["CAT","VAR"], ["TYPE","INT"]],
+
+	// FUNC
+	[["KEY", "action"],				["VAL", ["sentiment","look"]],
+														["ONASK",BOT_printActionList]],
+
+	[["KEY", "sentiment"], 		["VAL", "sentimentAurelie"], ["CAT", "ACT"],
+														["EFFECT","I will give you my sentiment about the house"]],
+
+	[["KEY", "look"], 				["VAL", "lookAurelie"], ["CAT", "ACT"],
+														["EFFECT","I will look at the elements in the room you talked about."]],
 	
 	// PREFS
-	[["KEY", "preference"],		["VAL", []], ["CAT","VAR"], ["ONASK",BOT_printPreferenceList]],  
-	[["KEY", "distaste"],			["VAL", []],  ["CAT","VAR"],["ONASK",BOT_printDistasteList]], 
-	[["KEY", "suggestion"],		["VAL", []], ["CAT","VAR"], ["ONASK",BOT_printSuggestionList]], 
-	[["KEY", "intention"],		["VAL", []], ["CAT","VAR"], ["ONASK",BOT_printIntentionList]],  
+	[["KEY", "preference"],		["VAL", [["houseTopic","first bedroom"],["houseTopic","livingroom"]]], ["CAT","VAR"], ["ONASK",BOT_printPreferenceList]],  
+	[["KEY", "distaste"],			["VAL", [["houseTopic","kitchen"],["houseTopic","exterior"]]],  ["CAT","VAR"],["ONASK",BOT_printDistasteList]],
 ];
 
 
@@ -100,26 +95,23 @@ var jeremyTopic = [
 	[["KEY", "_class"],							["VAL", "bot"], ["BOT","jeremyBot"]],
 	[["KEY", "_reference"],					["VAL", ["j","jeremy","son","boy"]]],
 	[["KEY", "_htmlprefix"],				["VAL", "jeremy"]], //prefix of HTML elements  
-	[["KEY", "_read"],							["VAL", ["userTopic","marcTopic","aurelieTopic"]]],
-	[["KEY", "_write"],							["VAL", ["userTopic"]]],
-	[["KEY", "_exec"],							["VAL", ["userTopic"]]], // try
+	[["KEY", "_read"],							["VAL", ["houseTopic","userTopic","marcTopic","aurelieTopic"]]],
 	
 	// BASICS INFOS
 	[["KEY", "type"],								["VAL", ["human","child"]]],
 	
-	[["KEY", "name"],								["VAL", "Jérémy"],   
-																	["ONASK", "My name is Jérémy. Best name ever."], 				
-																	["WHY","my parents gave it to me. Actually, I am very happy about it."]],
+	[["KEY", "name"],								["VAL", "Jérémy"],			
+																	["WHY","It's the best name ever"]],
 																	
 	[["KEY", "age"],								["VAL", 6], ["TYPE","INT"],
-																	["ONASK", "I am six year old."], 
+																	["ONASK", "I am 6 year old."], 
 																	["WHY","I don't know."]],
 																	
 	[["KEY", "gender"],							["VAL", "male"],
 																	["ONASK", function(s) { return ((s == "male") ? "I am boy!" : "A girl.") }]],
 																	
 	[["KEY", ["job"]],			        ["VAL", "What ?"]],
-	[["KEY", ["home","location"]],	["VAL", "My house is in San Diego"]],
+	[["KEY", ["home","location"]],	["VAL", "USA"]],
 	
 	// RELATIONS
 	[["KEY", "relative"],						["VAL", ["father","mother"]],
@@ -127,21 +119,47 @@ var jeremyTopic = [
 																	 
 	[["KEY", "father"],						["VAL", "marcTopic"],["CAT","REL"]],
 	[["KEY", "mother"],						["VAL", "aurelieTopic"],["CAT","REL"]],
+
+	// FUNC
+	[["KEY", "action"],				["VAL", ["play","show_joy_dad","show_joy_mom"]],
+														["ONASK",BOT_printActionList]],
+
+	[["KEY", "play"], 											["VAL", "play"], ["CAT", "ACT"],
+																					["EFFECT","I will play here and be joyful if I liked it."]],
+	[["KEY", ["showdad","show_joy_dad"]], 	["VAL", "showDad"], ["CAT", "ACT"],
+																					["EFFECT","I will tell my dad if I am joyful or not."]],
+	[["KEY", ["showmom","show_joy_mom"]], 	["VAL", "showMom"], ["CAT", "ACT"],
+																					["EFFECT","I will tell my mom if I am joyful or not."]],														
 	
 	// FEELINGS
-	[["KEY", "happiness"],		["VAL",  0.8], ["CAT","VAR"], ["TYPE","INT"]], // 7 standard feelings initiated
-	[["KEY", "confidence"],		["VAL", -0.8], ["CAT","VAR"], ["TYPE","INT"]],
-	[["KEY", "irritability"],	["VAL",  0.8], ["CAT","VAR"], ["TYPE","INT"]],
-	[["KEY", "satisfaction"],	["VAL", -0.8], ["CAT","VAR"], ["TYPE","INT"]],
-	[["KEY", "respect"],			["VAL", -0.8], ["CAT","VAR"], ["TYPE","INT"]],
-	[["KEY", "force"],				["VAL", 0], ["CAT","VAR"], ["TYPE","INT"]],
-	[["KEY", "excitement"],		["VAL", 0], ["CAT","VAR"], ["TYPE","INT"]],
+	[["KEY", ["joy","joyful"]],		["VAL",  0], ["CAT","VAR"], ["TYPE","INT"]], // de -10 à 10
 	
 	// PREFS
-	[["KEY", "preference"],		["VAL", []], ["CAT","VAR"], ["ONASK",BOT_printPreferenceList]],  
-	[["KEY", "distaste"],			["VAL", []],  ["CAT","VAR"],["ONASK",BOT_printDistasteList]], 
-	[["KEY", "suggestion"],		["VAL", []], ["CAT","VAR"], ["ONASK",BOT_printSuggestionList]], 
-	[["KEY", "intention"],		["VAL", []], ["CAT","VAR"], ["ONASK",BOT_printIntentionList]],  
+	[["KEY", "preference"],		["VAL", [["houseTopic","second bedroom"],["houseTopic","exterior"]]], ["CAT","VAR"], ["ONASK",BOT_printPreferenceList]],  
+	[["KEY", "distaste"],			["VAL", [["houseTopic","kitchen"],["houseTopic","bathroom"]]],  ["CAT","VAR"],["ONASK",BOT_printDistasteList]],
+];
+
+var houseTopic = [
+	// INFO 
+	[["KEY", "_class"],				["VAL", "house"]],
+	[["KEY", "_reference"],		["VAL", ["house","domicile"]]],
+	[["KEY", "type"],					["VAL", ["place"]]],
+
+	// VAR 
+	[["KEY", "name"],			["VAL", "House"], ["CAT","VAR"],
+												["WHY", "It is just a house"]],
+
+	[["KEY", "room"],		["VAL", ["bathroom","exterior","first bedroom","kitchen","livingroom","second bedroom"]], ["CAT","VAR"]],
+];
+
+var userTopic = [
+	// INFO 
+	[["KEY", "_class"],				["VAL", "user"]],
+	[["KEY", "_reference"],		["VAL", ["me","my","user"]]],
+	[["KEY", "type"],					["VAL", ["person"]]],
+	// VAR 
+	[["KEY", "name"],			["VAL", "Vendor"], ["CAT","VAR"],
+												["WHY", "I can't tell my real name."]]
 ];
 
 
@@ -150,16 +168,42 @@ var marcBot    	= new BOT_makeBot("marcBot","marcTopic");
 var aurelieBot  = new BOT_makeBot("aurelieBot","aurelieTopic");
 var jeremyBot   = new BOT_makeBot("jeremyBot","jeremyTopic");
 
-BOT_declareTopics(["marcTopic","aurelieTopic","jeremyTopic"]);
+BOT_declareTopics(["userTopic","houseTopic"]);
 
-BOT_theBotId		= "marcBot";		// sets current bot id
-
+BOT_theBotId				= "marcBot";		// sets current bot id
+BOT_theTopicId			= "marcTopic";	// sets current topic id
+BOT_theUserTopicId	= "userTopic";	// sets topic of current user id
 
 function BOT_onSwitchBot(oldbotid,newbotid) {
 	BOT_standardFrameBot(oldbotid, "visible", "4px solid #899DA3");
 	BOT_standardFrameBot(newbotid, "visible","4px solid #FFB030");
 }
 
+// ACTIONS
+function sentimentMarc(){
+	return "I am convinced at "+marcWidth+" %";
+}
 
+function lookMarc(){
+	return "TODO";
+}
 
+function sentimentAurelie(){
+	return "I am convinced at "+aurelieWidth+" %";
+}
 
+function lookAurelie(){
+	return "TODO";
+}
+
+function play(){
+
+}
+
+function showDad(){
+	
+}
+
+function showMom(){
+	
+}
---
+++ miniProjet/litetalk1.1/Visimmo/visimmoGame.code.js
@@ -0,0 +1,182 @@
+// ========= GAME parameters =========
+var timeInRooms = 60;  // s
+var currentTime;       // s
+var timerInterval;
+
+var roomsCount        = 6;
+var currentRoom;
+
+var roomNumberToFile  = new Array(roomsCount);
+roomNumberToFile[0]   = 'picts/exterior.jpg';
+roomNumberToFile[1]   = 'picts/livingroom.jpg';
+roomNumberToFile[2]   = 'picts/kitchen.jpg';
+roomNumberToFile[3]   = 'picts/bedroom1.jpg';
+roomNumberToFile[4]   = 'picts/bedroom2.jpg';
+roomNumberToFile[5]   = 'picts/bathroom.jpg';
+
+var roomNumberToName  = new Array(roomsCount);
+roomNumberToName[0]   = 'exterior';
+roomNumberToName[1]   = 'livingroom';
+roomNumberToName[2]   = 'kitchen';
+roomNumberToName[3]   = 'first bedroom';
+roomNumberToName[4]   = 'second bedroom';
+roomNumberToName[5]   = 'bathroom';
+
+var marcWidth;
+var marcBarInterval;
+var aurelieWidth;
+var aurelieBarInterval;
+
+// ========= FUNCTIONS =========
+
+function updateTimer(){
+  if(currentTime==-1){
+    nextRoom();
+  }
+  else {
+    document.getElementById("message_div").innerHTML = "Remaining time here: "+currentTime+" s";
+    currentTime--;
+  }
+}
+
+function nextRoom(){
+  currentRoom++;
+
+  // Show next room & start timer
+  if(currentRoom<roomsCount){
+    document.getElementById("room").src=roomNumberToFile[currentRoom];
+    document.getElementById("room_name").innerHTML = "<strong>Current step: "+roomNumberToName[currentRoom]+"</strong></br>";
+
+    if(timerInterval!=null){
+      window.clearInterval(timerInterval);
+    }
+    currentTime = timeInRooms;
+    updateTimer();
+    timerInterval = setInterval(updateTimer,1000);
+  } 
+  // End of game
+  else {
+    endGame();
+  } 
+}
+
+function getRoom(){
+  if(currentRoom==-1 || currentRoom>=roomsCount){
+    return "None";
+  }
+  return roomNumberToName[currentRoom];
+}
+
+function move(progressBar, diff) {
+    // Get moving direction
+    var sens = 1;
+    if(diff<0){
+      sens = -1;
+    }
+
+    // Get actual width and update it
+    var width;
+    if(progressBar=="aurelieBar"){
+      if(aurelieBarInterval!=null){
+        clearInterval(aurelieBarInterval);
+      }
+      width = aurelieWidth;
+      aurelieWidth+=diff;
+      aurelieWidth = Math.min(100,aurelieWidth);
+      aurelieWidth = Math.max(0,aurelieWidth);
+    }
+    else {
+      if(marcBarInterval!=null){
+        clearInterval(marcBarInterval);
+      }
+      width = marcWidth;
+      marcWidth+=diff;
+      marcWidth = Math.min(100,marcWidth);
+      marcWidth = Math.max(0,marcWidth);
+    }
+
+    // Update animation
+    var id;
+    if(progressBar=="aurelieBar"){
+      aurelieBarInterval = setInterval(frame, 20);
+      id = aurelieBarInterval;
+    }
+    else {
+      marcBarInterval = setInterval(frame, 20);
+      id = marcBarInterval;
+    }
+
+    var elem = document.getElementById(progressBar);
+    function frame() {
+      console.log("$$$$"+width)
+      console.log(diff)
+        if (width > 100 || width <0 || diff==0) {
+            clearInterval(id);
+        }
+        else {
+            width+=sens;
+            diff+=sens*-1;
+            elem.style.width = width + '%';
+        }
+
+        if(progressBar=="aurelieBar"){
+          document.getElementById("goal2").checked=(width==100);
+        }
+        else {
+          document.getElementById("goal1").checked=(width==100);
+        }
+    }
+}
+
+function startGame(){
+  // Make checkboxs unclickable
+  document.getElementById("goal1").disabled = true;
+  document.getElementById("goal2").disabled = true;
+  document.getElementById("goal1").checked = false;
+  document.getElementById("goal2").checked = false;
+
+  // start input
+  document.getElementById("litetalkchatbox").disabled = false;
+
+  // Set first room
+  currentRoom = -1;
+  nextRoom();
+
+  // Set progress bars
+  marcWidth    = 50;
+  aurelieWidth = 50;
+  document.getElementById("aurelieBar").style.width = aurelieWidth+"%";
+  document.getElementById("marcBar").style.width = marcWidth+"%";
+}
+
+function endGame(){
+  // Stop timers
+  window.clearInterval(timerInterval);
+
+  // Block input
+  document.getElementById("litetalkchatbox").disabled = true;
+
+  // Won or Lost ?
+  var won = document.getElementById("goal1").checked == true;
+  won = won && document.getElementById("goal2").checked == true;
+
+  // Message
+  var text;
+  if(won){
+    text = "You won! Play Again ?";
+  }
+  else {
+    text = "You lost! Retry ?";
+  }
+
+  // Show dialog
+  if (confirm(text)) {
+    startGame();
+  } else {
+    // Nothing
+  }
+}
+
+
+// ========= MAIN =========
+startGame();
